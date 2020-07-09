f43c42fd6b42e6d430f9964c6ddc5397626fbc94
colsonisaiah <colson.isaiah@gmail.com>
2020-07-09 09:41:26 -0400

    add config routes

---
+++ src/config/routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+function Routes(props) {
+    return(
+        <Switch>
+
+        </Switch>
+    );
+}
+
+export default Routes;
