6414d6fe07f73e80ba8f34c2b745e837c080bfdb
colsonisaiah <colson.isaiah@gmail.com>
2020-07-10 09:54:04 -0400

    hide parts of nav for users/ non-users

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = (props) => {
     return(
         <header>
-            <NavLink to='/titles'>Home</NavLink>
-            <NavLink to='/profile'>Profile</NavLink>
             <NavLink to='/'>Landing</NavLink>
-            <NavLink to='/signup'>Sign Up</NavLink>
-            <NavLink to='/login'>Log In</NavLink>
+            <NavLink to='/titles'>Home</NavLink>
+
+            {props.currentUser ? 
+                <>
+                    <NavLink to='/profile'>Profile</NavLink>
+                    <a href='/logout' onClick={props.logout}>Log Out</a>
+                </>
+            :
+                <>
+                    <NavLink to='/signup'>Sign Up</NavLink>
+                    <NavLink to='/login'>Log In</NavLink>
+                </>
+            }
         </header>
     );
 }
