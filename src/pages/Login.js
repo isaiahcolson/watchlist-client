198602b6995b16f027c289234d65cffb3e0687c4
colsonisaiah <colson.isaiah@gmail.com>
2020-07-09 21:33:24 -0400

    add content to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,45 @@
 import React from 'react';
 
-const Login = () => {
-    return(
-        <div>
-            <h1>You are on the login page.</h1>
-        </div>
-    );
+class Login extends React.Component {
+    state = {
+        username: "",
+        password: ""
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    handleSubmit = (event) => {
+        TODO
+    }
+
+    render() {
+        return(
+            <div>
+                <p>login</p>
+                <form onSubmit={this.handleSubmit}>
+                    <p>Username</p>
+                    <input onChange={this.handleChange}
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={this.state.username} />
+
+                    <p>Password</p>
+                    <input onChange={this.handleChange}
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={this.state.password} />
+
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default Login;
